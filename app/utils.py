dc5921aa2f07f414308b919f64afc1addeabdb69
blairt001 <blairt371.dev@gmail.com>
2019-01-20 00:52:01 +0300

    [fix #163345371]Fix feedback , add more validation functions

--- app/utils.py
+++ app/utils.py
@@ -6,8 +6,12 @@ from functools import wraps
 import jwt
 from flask import jsonify, request, abort, make_response
 from werkzeug.security import generate_password_hash
+from datetime import datetime
+import jwt
+
 
 # local imports
+from app.admin.models import Token
 from app.admin.models import UserModel
 from app.admin.db import select_from_db
 
@@ -20,25 +24,25 @@ def check_password(password, confirmed_password):
     '''
         # check to confirm the password is of required length
     if len(password) < 8 or len(password) > 20:
-        abort(make_response(jsonify(error="Password should not be less than 8 characters or exceed 20"), 400))
+        abort(make_response(jsonify(status=400, error="Password should not be less than 8 characters or exceed 20"), 400))
 
     # check if password contains at least an alphabet(a-z)
     if not re.search("[a-z]", password):
-        abort(make_response(jsonify(error="Password should contain a letter between a-z"), 400))
+        abort(make_response(jsonify(status=400, error="Password should contain a letter between a-z"), 400))
 
     # check if password contains at least an upper case letter
     if not re.search("[A-Z]", password):
-        abort(make_response(jsonify(error="Password should contain a capital letter"), 400))
+        abort(make_response(jsonify(status=400, error="Password should contain a capital letter"), 400))
 
     # check if password contains at least a number(0-9)
     if not re.search("[0-9]", password):
-        abort(make_response(jsonify(error="Password should contain a number(0-9)"), 400))
+        abort(make_response(jsonify(status=400, error="Password should contain a number(0-9)"), 400))
 
     # Checks if passwords provided by the users match
     if password != confirmed_password:
-        abort(make_response(jsonify(error="Your passwords don't match!"), 400))
+        abort(make_response(jsonify(status=400, error="Your passwords don't match!"), 400))
 
-    # If they match..
+    # hash the password with sha256 algorithm.
     hashed_password = generate_password_hash(password, method='sha256')
 
     return hashed_password
@@ -48,23 +52,38 @@ def validate_email(email):
     """
     Is the email valid , is it already used?
     """
-
     try:
         user, domain = str(email).split("@")
+        if not re.match("^[A-Za-z]*$", user):
+            abort(make_response(jsonify({
+                "status": 400, "Error":  "Email is Invalid"}), 400))
     except ValueError:
-        abort(make_response(jsonify(error="Email is Invalid"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Invalid Email"), 400))
     if not user or not domain:
         abort(make_response(jsonify(error="Email is Invalid"), 400))
 
-    # Is the domain you are using valid?
+    # Check that domain is valid
     try:
-        dom1, dom2 = domain.split(".")
+        dom_1, dom_2 = domain.split(".")
+        if not re.match("^[A-Za-z]*$", dom_1):
+            abort(make_response(jsonify({
+                "status": 400, "Error":  "Email is Invalid"}), 400))
+        if not re.match("^[A-Za-z]*$", dom_2):
+            abort(make_response(jsonify({
+                "status": 400, "Error":  "Email is Invalid"}), 400))
     except ValueError:
-        abort(make_response(jsonify(error="Email is Invalid"), 400))
-    if not dom1 or not dom2:
-        abort(make_response(jsonify(error="Email is Invalid"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Email is Invalid"), 400))
+    if not dom_1 or not dom_2:
+        abort(make_response(jsonify(
+            status=400,
+            error="Email is Invalid"), 400))
 
     return email
+
 
 #wrap our function and check for the access-token
 
@@ -76,6 +95,11 @@ def token_required(f):
             token = request.headers['x-access-token']
         if not token:
             return jsonify({'message':"Token is missing"}), 401
+
+        blacklisted = Token.check_if_token_blacklisted(token)
+        if blacklisted:
+            abort(make_response(jsonify({
+                'message':'Please login again to continue'}), 401))
 
         try:
             data = jwt.decode(token, key)
@@ -90,6 +114,7 @@ def token_required(f):
 
         return f(current_user, *args, **kwargs)
     return decorated
+
 
 #lets decode our token back
 def decode_token():
@@ -159,6 +184,26 @@ def check_if_string(data):
             "status": 400,
             "Error":  "Make sure you only use letters in your username"}), 400))
 
+def check_for_whitespace(data):
+    """
+    Check for whitespace only in input data
+    """
+    for keys, value in data.items():
+        if not value.strip():
+            abort(make_response(jsonify({
+                'status': 400,
+                'error':'{} field cannot be left blank'.format(keys)}), 400))
+
+    return True
+
+def check_if_string(data):
+    for key, value in data.items():
+        if key in ['firstname', 'lastname', 'username']:
+            if not value.isalpha():
+                abort(make_response(jsonify({
+                    "status": 400,
+                    "Error":  "Make sure you only use letters in your {}".format(key)}), 400))
+
 
 def check_phone_number(phone):
     if not re.match('^[0-9]*$', phone):
@@ -170,3 +215,34 @@ def check_phone_number(phone):
         abort(make_response(jsonify({
             "status": 400,
             "Error":  "Phone number should be 10 digits."}), 400))
+
+
+def check_date(date):
+    if not re.match(r"^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/([12][0-9]{3})$", date):
+        abort(make_response(jsonify({
+            "status": 400, "Error":  "Invalid date format. Should be DD/MM/YYYY"}), 400))
+
+    date_format = "%d/%m/%Y"
+    # create datetime objects from the strings
+    strpdate = datetime.strptime(date, date_format)
+    now = datetime.now()
+
+    if strpdate < now:
+        abort(make_response(jsonify({
+            "status": 400, "Error":  "Date should be in the future"}), 400))
+
+    months = ['Jan', 'Feb', 'March', 'April', 'May',
+              'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+    format_date = date[0:2]+ " " + months[int(date[3:5]) -1] + " " + date[6:]
+    return format_date
+
+#has the user already responded "yes" to the meetup
+def check_if_already_rsvpd(meetup_id, user_id):
+    query = """
+    SELECT rsvp_id FROM rsvps
+    WHERE rsvps.meetup_id = '{}' AND rsvps.user_id = '{}'
+    AND rsvps.rsvp = '{}'
+    """.format(meetup_id, user_id, 'yes')
+
+    rsvp = select_from_db(query)
+    return rsvp
